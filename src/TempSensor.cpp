6e005b508c26a60cd6b49fd79ee4dc3ad1b21d17
Michael Haas <michael.haas@computerlinguist.org>
2017-12-07 07:08:34 +0100

    Read fault register of temperature sensor

--- src/TempSensor.cpp
+++ src/TempSensor.cpp
@@ -17,17 +17,31 @@ void TempSensor::update() {
   int now = millis();
   if (now > lastRead + TEMP_INTERVALL) {
     lastRead = now;
+
     double rtd = sensor->readRTD();
     DEBUG.print("RTD: ");
     DEBUG.println(rtd);
     float temperature = sensor->temperature(R_AT_ZERO_DEGREES, R_REFERENCE);
+    DEBUG.print("temperature:");
+    DEBUG.println(temperature);
+
     if (temperature < -50 || temperature > 200) {
       // Indicate that this is a bogus reading.
       temperature = INVALID_READING;
       DEBUG.println("TempSensor: Invalid reading detected!");
     }
-    DEBUG.print("temperature:");
-    DEBUG.println(temperature);
+    uint8_t fault = sensor->readFault();
+    if (fault != 0) {
+      temperature = INVALID_READING;
+      DEBUG.print("TempSensor: fault detected!");
+      DEBUG.println(fault);
+      // TODO: does the Adafruit driver clear this bit?
+      // I'd rather disable everything forever.
+      // TODO: the Adafruit driver just reads the fault register without initiating
+      // the fault detection cycle. This means the example on the Adafruit site is
+      // not quite correct: some conditions checked there can only be
+      // detected when a fault detection cycle is initiated.
+    }
     settings->setCurrentTemperature(temperature);
   }
 }
